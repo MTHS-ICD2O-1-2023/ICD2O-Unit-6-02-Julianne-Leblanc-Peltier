c246a8c323fccfbd39e0aa9cada7b92def319367
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-06-03 02:46:51 +0000

    Cookies

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Julianne Leblanc-Peltier
+// Created on: June 2024
 // This file contains the JS functions for index.html
+
+let cookiesClicked = 0
+
+function bigCookie() {
+  if (localStorage.cookiesClicked) {
+    localStorage.cookiesClicked = Number(localStorage.cookiesClicked) +1
+  } else {
+    localStorage.cookiesClicked = 1
+  }
+  document.getElementById("amount-of-cookies").innerHTML = "Cookies clicked: " + cookiesClicked
+}
+
